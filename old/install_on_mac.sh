5e7fccf6b3850521a6557c742acdac8bc42a57a7
pverkind <peter.verkinderen@gmail.com>
2021-03-03 14:08:44 +0100

    Remove old folder and add .gitignore

--- old/install_on_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd -- "$(dirname "$BASH_SOURCE")" 
-mkdir -p "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-cp ./OpenITImARkdown.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-echo "OpenITImARkdown highlighting scheme for Kate installed."
-echo "Close Kate and (re-)open it for the changes to take effect."
