00dc5ba06195e0bc4804cd1e18e5b2f485e5472e
Maxim Romanov <romanov.maxim@gmail.com>
2022-09-27 09:42:21 +0200

    update

--- install_on_windows.bat
+++ install_on_windows.bat
@@ -1,2 +1,3 @@
 if not exist %LocalAppData%\org.kde.syntax-highlighting\syntax mkdir %LocalAppData%\org.kde.syntax-highlighting\syntax
 copy /Y .\OpenITImARkdown.xml %LocalAppData%\org.kde.syntax-highlighting\syntax
+copy /Y .\OpenITImARkdown_EIS1600.xml %LocalAppData%\org.kde.syntax-highlighting\syntax
