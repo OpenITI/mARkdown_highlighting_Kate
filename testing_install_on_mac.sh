0026b15f2e6db6085f6df6eaf68d7a151f60b7b1
Maxim Romanov <romanov.maxim@gmail.com>
2022-10-05 14:25:55 +0200

    minor fix for EIS1600 scheme

--- testing_install_on_mac.sh
+++ testing_install_on_mac.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd -- "$(dirname "$BASH_SOURCE")" 
 mkdir -p "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-cp ./OpenITImARkdown.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
-cp ./OpenITImARkdown_EIS1600.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
+cp ./SCHEME_FILES/OpenITImARkdown.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
+cp ./SCHEME_FILES/OpenITImARkdown_EIS1600.xml "$HOME/Library/Application Support/org.kde.syntax-highlighting/syntax"
 echo "OpenITImARkdownSH highlighting scheme for Kate installed."
 echo "Close Kate and (re-)open it for the changes to take effect."
 
