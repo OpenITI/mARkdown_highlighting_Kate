54c2a196b6aa3ddd0f36ae843f4b78c157245a1a
pverkind <peter.verkinderen@gmail.com>
2025-03-12 19:11:17 +0100

    Add quotation marks around the path

--- install_on_windows.bat
+++ install_on_windows.bat
@@ -1,3 +1,3 @@
-if not exist %LocalAppData%\org.kde.syntax-highlighting\syntax mkdir %LocalAppData%\org.kde.syntax-highlighting\syntax
-copy /Y .\SCHEME_FILES\OpenITImARkdown.xml %LocalAppData%\org.kde.syntax-highlighting\syntax
-copy /Y .\SCHEME_FILES\OpenITImARkdown_EIS1600.xml %LocalAppData%\org.kde.syntax-highlighting\syntax
+if not exist "%LocalAppData%"\org.kde.syntax-highlighting\syntax mkdir "%LocalAppData%"\org.kde.syntax-highlighting\syntax
+copy /Y .\SCHEME_FILES\OpenITImARkdown.xml "%LocalAppData%"\org.kde.syntax-highlighting\syntax
+copy /Y .\SCHEME_FILES\OpenITImARkdown_EIS1600.xml "%LocalAppData%"\org.kde.syntax-highlighting\syntax
